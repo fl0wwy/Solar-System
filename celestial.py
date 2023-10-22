b9adcb3ad11e33eab77a238d17d570653b428fa2
Bar Ben Waiss <psnoob510@gmail.com>
2023-10-22 23:27:27 +0300

    initial commit

---
+++ celestial.py
@@ -0,0 +1,101 @@
+from abc import ABC, abstractmethod
+import pygame as pg
+import math
+
+class Celestial(ABC):
+    AU = 149600000000 # 1AU km's
+    TIMESTEP = 3600*24 # seconds in a day
+    G = 6.67430e-11 
+
+    def __init__(self, x, y, mass, radius, display, color) -> None:
+        super().__init__()
+
+        self.mass = mass
+        self.radius = radius
+        self.x = x * Celestial.AU
+        self.y = y * Celestial.AU
+        self.distance = 0
+
+        self.orbit = [] # Track orbit points
+        self.vel_x = 0
+        self.vel_y = 0
+
+        self.display = display
+        self.color = color
+
+        self.SCALE = 1 # arbitrary constant number to scale actual radius of the celestial object to fit the simulation dimensions
+
+    def draw(self):
+        x = self.x / 1496000000 + (self.display.get_width() / 2)
+        y = self.y / 1496000000 + (self.display.get_height() / 2) 
+        pg.draw.circle(self.display, self.color, (x, y), self.radius / self.SCALE)      
+
+
+class Star(Celestial):
+    def __init__(self, x, y, mass, radius, display, color) -> None:
+        super().__init__(x, y, mass, radius, display, color)
+        self.SCALE = 30000
+
+
+class Planet(Celestial):
+    def __init__(self, x, y, mass, radius, display, color, velocity) -> None:
+        super().__init__(x, y, mass, radius, display, color)
+        
+        self.SCALE = 500
+        self.vel_y = velocity * 1000 
+
+    def calc_attraction(self, celestial):
+        dist_x = celestial.x - self.x
+        dist_y = celestial.y - self.y
+        r = math.sqrt(dist_x**2 + dist_y**2)
+
+        if isinstance(celestial, Star):
+            self.distance = r
+            
+        force = (Celestial.G * self.mass * celestial.mass) / r**2
+        alpha = math.atan2(dist_y, dist_x)
+        force_x = math.cos(alpha) * force
+        force_y = math.sin(alpha) * force  
+        return force_x, force_y
+
+    def update_position(self, celestials):
+        total_fx = total_fy = 0
+        for body in celestials:
+            if body != self:
+                fx, fy = self.calc_attraction(body) 
+                total_fx += fx
+                total_fy += fy
+            
+        # calculating velocity 
+        self.vel_x += total_fx / self.mass * self.TIMESTEP   
+        self.vel_y += total_fy / self.mass * self.TIMESTEP
+
+        # updating position
+        self.x += self.vel_x * self.TIMESTEP
+        self.y += self.vel_y * self.TIMESTEP
+        self.orbit.append((self.x, self.y))
+
+    def draw_orbit(self):
+        if len(self.orbit) > 2:
+            updated_points = []
+            for point in self.orbit:
+                x, y = point
+                x = x / 1496000000 + (self.display.get_width() / 2)
+                y = y / 1496000000 + (self.display.get_height() / 2)   
+                updated_points.append((x, y))
+            pg.draw.lines(self.display, self.color, False, updated_points, 3) 
+
+        if len(self.orbit) >= int(100 * (self.radius/6371)):
+            self.orbit.pop(0)     
+
+    def update(self, celestials):
+        self.draw_orbit()
+        self.draw()    
+        self.update_position(celestials)
+        
+
+class Moon(Celestial):
+    def __init__(self, x, y, mass, radius, position, display, color) -> None:
+        super().__init__(x, y, mass, radius, position, display, color)
+
+              
---
+++ main.py
@@ -0,0 +1,50 @@
+from celestial import *
+
+class Simulation:
+    def __init__(self) -> None:
+        self.WIN = pg.display.set_mode((2000, 800))
+        self.CLOCK = pg.time.Clock()
+        pg.display.set_caption("Solar System")
+
+        self.sun = Star(0, 0, 1.989 * 10**30, 695950, self.WIN, '#FFCC33')
+        self.earth = Planet(-1, 0, 5.972 * 10**24, 6052, self.WIN, 'deepskyblue3', 29.78)
+        self.mars = Planet(-1.52, 0, 0.64 * 10**24, 3390, self.WIN, '#c1440e', 24)
+        self.saturn = Planet(9.54, 0, 586 * 10**24, 9000, self.WIN, '#ceb8b8', 9.7)
+        self.mercury = Planet(0.39, 0, 0.33 * 10 ** 24, 2438, self.WIN, '#8c8c94', 47.9)
+        self.venus = Planet(0.72, 0, 4.87 * 10**24, 6052, self.WIN, '#e39e1c', 35)
+        self.jupiter = Planet(-5.2, 0, 1898.60 * 10**24, 10000, self.WIN, '#d0a47a', 13.1)
+        self.uranus = Planet(19.2, 0, 86.62 * 10**24, 8000, self.WIN, '#8dc9ee', 6.8)
+        self.neptune = Planet(-30.06, 0, 102.42 * 10**24, 7500, self.WIN, '#1f2255', 5.4) 
+
+
+        self.bodies = [self.earth, self.mars, self.sun, self.saturn, self.mercury,
+                        self.venus, self.jupiter, self.uranus, self.neptune]
+
+
+    def update_sim(self):
+        pg.display.update()
+        self.CLOCK.tick(60)
+
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                exit()
+
+    def run(self):
+        while True:
+            self.check_events()
+            self.WIN.fill('black')
+
+            for body in self.bodies:
+                body.draw()
+                if isinstance(body, Planet):
+                    body.update(self.bodies)
+
+            self.update_sim()                    
+
+
+
+if __name__ == "__main__":
+    sim = Simulation()
+    sim.run()
